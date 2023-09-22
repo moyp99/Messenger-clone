7d1a2f65693cbd74fcd28ddc23abebe19cf741d4
moyp99 <moypg99@gmail.com>
2023-09-22 14:22:19 -0600

    (fix) fixed messages list order, fixed message list height)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 import { FormControl, Input } from "@material-ui/core";
 import Message from "./Message";
@@ -9,37 +9,41 @@ import SendIcon from "@material-ui/icons/Send";
 import { IconButton } from "@material-ui/core";
 
 function App() {
+  const ref = useRef(null);
   const [input, setInput] = useState("");
-  const [messages, setMessages] = useState([
-    { username: "Qazi", message: "hey guys" },
-    { username: "Mike", message: "hey guys" },
-  ]);
+  const [messages, setMessages] = useState([]);
   const [username, setUsername] = useState("");
 
-  // Every time that you neeed to change a variable you have to use setInput(<stuff>) or similar
-  //useState = wariable in React, it's a space in memory that will be erased when refresh.
-  // useEffect = run code on a condition in React
-
-  useEffect(
-    () => {
-      //Run code here...
-      //if its blank inside [] (dependencies), this code runs once when the app component loads
-      //if we have a cvariable like input, it will run the code every time that variable changes
-      setUsername(prompt("Please enter your name"));
-    },
-    //this is the condition
-    []
-  );
+  useEffect(() => {
+    setUsername(prompt("Please enter your name"));
+  }, []);
 
   useEffect(() => {
     db.collection("messages")
-      .orderBy("timestamp", "desc")
+      .orderBy("timestamp", "asc")
       .onSnapshot((snapshot) => {
         setMessages(
-          snapshot.docs.map((doc) => ({ id: doc.id, message: doc.data() }))
+          snapshot.docs
+            .map((doc) => ({
+              id: doc.id,
+              message: doc.data(),
+              timestamp: doc.timestamp,
+            }))
+            .sort((a, b) => {
+              return a.timestamp - b.timestamp;
+            })
         );
       });
   }, []);
+
+  useEffect(() => {
+    if (messages.length) {
+      ref.current?.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+      });
+    }
+  }, [messages.length]);
 
   const sendMessage = (event) => {
     event.preventDefault();
@@ -49,45 +53,51 @@ function App() {
       username: username,
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
     });
-
-    // setMessages([...messages, { username: username, message: input }]);
     setInput("");
   };
 
   return (
     <div className="App">
-      <img
-        className="app__logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Facebook_Messenger_logo_2020.svg/800px-Facebook_Messenger_logo_2020.svg.png"
-      />
-      <h1>Hello everybody, how is it going?</h1>
-      <h2>Welcome {username}</h2>
+      <div className="header">
+        <img
+          alt="app logo"
+          className="app__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Facebook_Messenger_logo_2020.svg/800px-Facebook_Messenger_logo_2020.svg.png"
+        />
+        <h1>Hello everybody, how is it going?</h1>
+        <h2>Welcome {username}</h2>
+      </div>
+      <div className="messages__container">
+        <div ref={ref} style={{ height: "fit-content" }}>
+          <FlipMove>
+            {messages.map(({ id, message }) => (
+              <Message key={id} username={username} message={message} />
+            ))}
+          </FlipMove>
+        </div>
+      </div>
+
       <form className="app__form">
         <FormControl className="app__formControl">
           <Input
-          className="app__input"
+            className="app__input"
             placeholder="Enter a message"
             value={input}
             onChange={(event) => setInput(event.target.value)}
           />
 
-          <IconButton className="app__iconButton"
+          <IconButton
+            className="app__iconButton"
             disabled={!input}
             variant="contained"
             color="primary"
             type="submit"
             onClick={sendMessage}
           >
-          
             <SendIcon />
           </IconButton>
         </FormControl>
       </form>
-      <FlipMove>
-        {messages.map(({ id, message }) => (
-          <Message key={id} username={username} message={message} />
-        ))}
-      </FlipMove>
     </div>
   );
 }
